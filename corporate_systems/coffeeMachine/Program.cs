9f6ea733c599cc257dcbb8cfe67e04c77bf64a0d
Slava Pogosov <pogoslav1@gmail.com>
2024-09-26 17:12:02 +0300

    update coffee

--- corporate_systems/coffeeMachine/Program.cs
+++ corporate_systems/coffeeMachine/Program.cs
@@ -14,45 +14,6 @@ while (true)
 
   if (choice == 0)
   {
-    break;
-  }
-
-  if (choice == 1)
-  {
-    if (water >= 300)
-    {
-      water -= 300;
-      americanoCount++;
-      totalEarnings += 150;
-      Console.WriteLine("Ваш напиток готов.");
-    }
-    else
-    {
-      Console.WriteLine("Не хватает воды.");
-    }
-  }
-  else if (choice == 2)
-  {
-    if (water >= 30 && milk >= 270)
-    {
-      water -= 30;
-      milk -= 270;
-      latteCount++;
-      totalEarnings += 170;
-      Console.WriteLine("Ваш напиток готов.");
-    }
-    else if (water < 30)
-    {
-      Console.WriteLine("Не хватает воды.");
-    }
-    else
-    {
-      Console.WriteLine("Не хватает молока.");
-    }
-  }
-
-  if (water < 30 && milk < 270)
-  {
     Console.WriteLine("*Отчёт*");
     Console.WriteLine($"Ингредиентов осталось: \nВода: {water} мл\nМолоко: {milk} мл");
     Console.WriteLine($"Кружек американо приготовлено: {americanoCount}");
@@ -60,13 +21,50 @@ while (true)
     Console.WriteLine($"Итого: {totalEarnings} рублей.");
     break;
   }
-}
 
-if (water >= 30 || milk >= 270)
-{
-  Console.WriteLine("*Отчёт*");
-  Console.WriteLine($"Ингредиентов осталось: \nВода: {water} мл\nМолоко: {milk} мл");
-  Console.WriteLine($"Кружек американо приготовлено: {americanoCount}");
-  Console.WriteLine($"Кружек латте приготовлено: {latteCount}");
-  Console.WriteLine($"Итого: {totalEarnings} рублей.");
+  if (choice == 1) // Американо
+  {
+    if (water >= 300)
+    {
+      water -= 300;
+      americanoCount++;
+      totalEarnings += 150;
+      Console.WriteLine("Ваш американо готов.");
+    }
+    else
+    {
+      Console.WriteLine("Не хватает воды для американо.");
+    }
+  }
+  else if (choice == 2) // Латте
+  {
+    if (water >= 30 && milk >= 270)
+    {
+      water -= 30;
+      milk -= 270;
+      latteCount++;
+      totalEarnings += 170;
+      Console.WriteLine("Ваш латте готов.");
+    }
+    else if (water < 30)
+    {
+      Console.WriteLine("Не хватает воды для латте.");
+    }
+    else
+    {
+      Console.WriteLine("Не хватает молока для латте.");
+    }
+  }
+
+  Console.WriteLine($"Остаток ингредиентов: \nВода: {water} мл\nМолоко: {milk} мл");
+  if (water < 30 && milk < 270)
+  {
+    Console.WriteLine("Ингредиентов недостаточно для приготовления напитков.");
+    Console.WriteLine("*Отчёт*");
+    Console.WriteLine($"Ингредиентов осталось: \nВода: {water} мл\nМолоко: {milk} мл");
+    Console.WriteLine($"Кружек американо приготовлено: {americanoCount}");
+    Console.WriteLine($"Кружек латте приготовлено: {latteCount}");
+    Console.WriteLine($"Итого: {totalEarnings} рублей.");
+    break;
+  }
 }
