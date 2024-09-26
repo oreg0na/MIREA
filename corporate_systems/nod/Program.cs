df9b0e32f391ec7c04662f41202db96687d01ba9
Slava Pogosov <pogoslav1@gmail.com>
2024-09-26 16:18:18 +0300

    add nod

---
+++ corporate_systems/nod/Program.cs
@@ -0,0 +1,31 @@
+﻿int GCD(int a, int b)
+{
+  return b == 0 ? a : GCD(b, a % b);
+}
+
+Console.Write("Введите числитель: ");
+int numerator = int.Parse(Console.ReadLine());
+
+Console.Write("Введите знаменатель: ");
+int denominator = int.Parse(Console.ReadLine());
+if (denominator == 0)
+{
+  Console.WriteLine("Ошибка: знаменатель не может быть равен 0.");
+}
+else
+{
+  int gcd = GCD(Math.Abs(numerator), Math.Abs(denominator));
+
+  // Упрощаем дробь
+  numerator /= gcd;
+  denominator /= gcd;
+
+  if (denominator < 0)
+  {
+    numerator = -numerator;
+    denominator = -denominator;
+  }
+
+  // Выводим результат
+  Console.WriteLine($"Результат: {numerator} / {denominator}");
+}
