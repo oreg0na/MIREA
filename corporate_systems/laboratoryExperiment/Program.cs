46fa491053781ae47f129930c821a3963bc85a80
Slava Pogosov <pogoslav1@gmail.com>
2024-09-26 17:27:17 +0300

    update

--- corporate_systems/laboratoryExperiment/Program.cs
+++ corporate_systems/laboratoryExperiment/Program.cs
@@ -5,34 +5,40 @@ class Program
   static void Main()
   {
     Console.WriteLine("*********************************************************");
+
     Console.Write("Введите количество бактерий: ");
     int bacteria = int.Parse(Console.ReadLine());
+
     Console.Write("Введите количество антибиотика: ");
     int antibiotics = int.Parse(Console.ReadLine());
+
     int hour = 0;
     int antibioticEffect = 10;
-    while (bacteria > 0 && antibiotics > 0)
+
+    while (bacteria > 0 && antibiotics > 0 && hour < 10)
     {
       hour++;
       bacteria *= 2;
-      bacteria -= antibioticEffect;
+      bacteria -= antibiotics * antibioticEffect;
+      if (bacteria <= 0)
+      {
+        Console.WriteLine($"После {hour} часа бактерий осталось 0");
+        Console.WriteLine("Колония вымерла.");
+        break;
+      }
+
+      Console.WriteLine($"После {hour} часа бактерий осталось {bacteria}");
       antibioticEffect--;
-      if (antibioticEffect <= 0)
+      if (antibioticEffect == 0)
       {
         antibiotics--;
         antibioticEffect = 10;
       }
-      if (bacteria < 0)
-      {
-        bacteria = 0;
-      }
-      Console.WriteLine($"После {hour} часа бактерий осталось {bacteria}");
     }
-    while (bacteria > 0)
+
+    if (bacteria > 0 && hour == 10)
     {
-      hour++;
-      bacteria *= 2;
-      Console.WriteLine($"После {hour} часа бактерий осталось {bacteria}");
+      Console.WriteLine("Колония прожила 10 часов.");
     }
   }
 }
