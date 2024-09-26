067002964cd22b9ab54b0337958139d0199f2a9e
Slava Pogosov <pogoslav1@gmail.com>
2024-09-26 16:52:02 +0300

    add

---
+++ corporate_systems/colonizationMars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+  static void Main()
+  {
+    Console.WriteLine("*********************************************************");
+    Console.Write("Введите n: ");
+    int n = int.Parse(Console.ReadLine());
+    Console.Write("Введите a: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Введите b: ");
+    int b = int.Parse(Console.ReadLine());
+    Console.Write("Введите w: ");
+    int w = int.Parse(Console.ReadLine());
+    Console.Write("Введите h: ");
+    int h = int.Parse(Console.ReadLine());
+    int maxD = FindMaxD(n, a, b, w, h);
+    Console.WriteLine($"Ответ d = {maxD}");
+  }
+  static int FindMaxD(int n, int a, int b, int w, int h)
+  {
+    int d = 0;
+    while (true)
+    {
+      int moduleWidth = a + 2 * d;
+      int moduleHeight = b + 2 * d;
+      int modulesPerWidth = w / moduleWidth;
+      int modulesPerHeight = h / moduleHeight;
+      if (modulesPerWidth * modulesPerHeight < n)
+      {
+        return d - 1;
+      }
+      d++;
+    }
+  }
+}
